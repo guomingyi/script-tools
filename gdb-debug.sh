4d32c780ee97fa010a541ab512ee9965d8b8594f
gmy <mingyi.guo@tinno.com>
2019-11-05 16:47:44 +0800

    <gdb + gdbserver 调试脚本第一版完成，极大提升调试效率>

--- gdb-debug.sh
+++ gdb-debug.sh
@@ -11,84 +11,156 @@ function echo_green()
     echo -e "\e[1;32m$*\e[0m"
 }
 
-
-mydir=$(dirname $0)
-#echo "mydir=$mydir"
-
-#被调试的可执行程序
-execfile=$1
-
-#符号信息目录
-symbols=$2
-
-#${string/%substring/replacement}
-symbols=${symbols/%\//''}
-#echo "symbols=$symbols"
-
-symbols_abs_path=$PWD/"$symbols/vendor"
-execfile_path=$PWD/`find $symbols -name $execfile`
-
-#search_path1=`find $symbols -name lib`
-search_path2=`find $symbols -name lib64`
-search_path3+=$search_path1
-search_path3+=$search_path2
-
-for i in $search_path3
-do
-j+="$PWD/$i:"
-done;
-
-symbols_search_path=${j/%\:/''}
-
-#echo "symbols_search_path=$symbols_search_path"
-#exit
-
-#echo "symbols_abs_path=$symbols_abs_path"
-#echo "execfile_path=$execfile_path"
-
-# 从前开始截取匹配第一个字符串
-execdest="${execfile_path#*system}"
-if [ "y$execdest" == "y$execfile_path" ]; then
-    execdest="${execfile_path#*vendor}"
-    if [ "y$execdest" == "y$execfile_path" ]; then
-        echo_red "not found dest:$execfile_path"
-	else
-		execdest="vendor$execdest"
+function reset_port()
+{
+    port=$1
+    pid=`ps aux |grep "$port" |grep "/data/gdbserver" |awk '{print $2}'`
+    if [ ! "y$pid" == "y" ]; then
+        echo "reset port: kill -9 $pid"
+        kill -9 $pid
     fi
-else
-    execdest="system$execdest"
+}
+
+function create_gdbinit_cmd()
+{
+    cmd="$1"
+    ssp="$2"
+    po="$3"
+    echo "define $cmd" > $GDBINIT
+    echo "set solib-search-path $ssp" >> $GDBINIT
+    echo "set solib-absolute-prefix $symbols" >> $GDBINIT
+    echo "target remote :$po" >> $GDBINIT
+    echo "tui enable" >> $GDBINIT
+    echo "end" >> $GDBINIT
+    echo "document $cmd" >> $GDBINIT
+    echo "Print gdb init set." >> $GDBINIT
+    echo "end" >> $GDBINIT
+}
+
+function do_pre_connect()
+{
+    po="$1"
+    dest="$2"
+    
+    ret=`adb root`
+    if [ "y$ret" == "y" ]; then
+        echo_red "ADB ROOT失败！"
+        exit 1
+    fi
+
+    adb push $GDB_server $GDB_SERVER_PATH && adb shell chmod +x $GDB_SERVER_PATH
+    echo_green "启动 gdbserver: adb shell $GDB_SERVER_PATH :$po $dest &"
+    adb shell $GDB_SERVER_PATH :$po $dest &
+    echo_green "TCP转发: adb forward tcp:$po tcp:$po"
+    adb forward tcp:$po tcp:$po
+}
+
+function get_execfile_name()
+{
+    path="$1"
+    execdest="${path#*system}"
+    if [ "y$execdest" == "y$path" ]; then
+        execdest="${path#*vendor}"
+        if [ "y$execdest" == "y$path" ]; then
+            echo_red "not found dest:$path"
+            exit 0
+        else
+            execdest="vendor$execdest"
+        fi
+    else
+        execdest="system$execdest"
+    fi
+}
+
+function get_so_path()
+{
+    syms=$1
+    j=""
+    search_lib_p=""
+    
+    syms=${syms/%\//''}
+    search_lib=`find $syms -name *.so`
+   
+    rm -rf ~/.slib
+    for i in $search_lib ; do
+        k=${i%/*}
+        echo $k >> ~/.slib
+    done
+    
+    search_lib_p=`sort -u ~/.slib`
+    for i in $search_lib_p ; do
+        j+="$PWD/$i:"
+    done;
+
+    symbols_search_path=${j/%\:/''}
+}
+
+function usage_print()
+{
+    echo "----------------------------------------------[使用说明]--------------------------------------------------"
+    echo "gdb & gdbserver 需要用NDK里面的!!!"
+    echo "./gdb-debug.sh [符号目录] [可执行程序名称]"
+    echo_green "./gdb-debug.sh out/target/product/v830/symbols vendor.tinno.hardware.FactoryServer@1.0-service"
+    echo "----------------------------------------------------------------------------------------------------------"
+}
+
+####################################【我是分割线】########################################
+symbols="$1"
+execfile="$2"
+
+#symbols=out/target/product/v830/symbols
+#execfile=vendor.tinno.hardware.FactoryServer@1.0-service
+
+if [ "y$execfile" = "y" ] || [ "y$symbols" = "y" ] ; then
+    usage_print;
+    exit 0;
 fi
 
-echo_green "execdest=$execdest"
+GDBTMP=~/.gdbtmp
+echo "symbols=$symbols" > $GDBTMP
+echo "execfile=$execfile" >> $GDBTMP
 
+NDK=/home/android/Android/android-ndk-r20
+GDB=$NDK/prebuilt/linux-x86_64/bin/gdb
+GDB_SERVER=$NDK/prebuilt/android-arm/gdbserver/gdbserver
+GDB_SERVER64=$NDK/prebuilt/android-arm64/gdbserver/gdbserver
+GDB_SERVER_PATH=/data/gdbserver
+GDB_server=$GDB_SERVER
+GDBINIT=~/.gdbinit
+port=1235
+execdest=""
+symbols_search_path=""
 
-# 准备工作.
-adb root && adb remount
-adb push $mydir/gdbserver64 system/bin/gdbserver64
+if [ ! -d $symbols ]; then
+    echo_red "符号目录输入错误: $symbols"
+    exit 0;
+fi
 
-#启动gdbserver和tcp转发.#加&开新线程，否则会阻塞.
-port=1779
-echo "adb shell system/bin/gdbserver64 :$port $execdest &"
-adb shell system/bin/gdbserver64 :$port $execdest &
+get_so_path $symbols
+if [ "y$symbols_search_path" = "y" ]; then
+    echo_red "获取symbols目录失败！"
+    exit 0;
+fi
+echo_green "获取依赖so目录集合..ok".
 
-echo "adb forward tcp:$port tcp:$port"
-adb forward tcp:$port tcp:$port
+execfile_path=`find $symbols -name $execfile`
+if [ "y$execfile_path" = "y" ]; then
+    echo_red "可执行程序输入错误: $execfile"
+    exit 0;
+fi
 
-#默认设置加载命令.
-#echo "target remote :$port" > ~/.gdbinit
-#echo "set solib-search-path $PWD/out/target/product/c330ae/symbols/vendor/lib64/" > ~/.gdbinit
-#echo "set solib-absolute-prefix $PWD/out/target/product/c330ae/symbols/" >> ~/.gdbinit
-#echo "-" >> ~/.gdbinit
+get_execfile_name $execfile_path
+echo_green "获取可执行程序路径..ok".
 
-#启动调试.一定要弄对gdb和gdbserver的版本对应关系，否则会链接失败!!!
-#  Remote side has terminated connection.  GDBserver will reopen the connection
+echo_green "准备调试..".
+reset_port $port
+do_pre_connect $port $execdest
 
-#cd /home/android/driver/c330/LA.UM.7.6.2/LINUX/android/prebuilts/gdb/linux-x86/bin
-#echo "$execfile_path"
+echo_green "GDB初始化命令: go [设置依赖so路径 启动tui 连接到远程服务..]"
+create_gdbinit_cmd "go" $symbols_search_path $port
 
-/home/android/driver/c330/LA.UM.7.6.2/LINUX/android/prebuilts/gdb/linux-x86/bin/gdb $execfile_path
-
-
+echo_green "启动GDB调试客户端: $GDB $execfile_path"
+$GDB $execfile_path
 
 
 
